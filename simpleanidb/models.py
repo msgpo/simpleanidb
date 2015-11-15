1c0ab9644ae939b92fb6d62ba343a6359f42ad74
Daniel O'Connell <tojad99+github@gmail.com>
2015-11-15 23:04:06 +0100

    Updated xml tag definitions
    It appears that anidb changed how they represent tags, which meant that
    Anime.load() would raise an exception.

--- simpleanidb/models.py
+++ simpleanidb/models.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
-from datetime import date
 import requests
 import xml.etree.ElementTree as ET
 
 from .helper import date_to_date
+
 
 class Anime(object):
 
@@ -39,9 +39,10 @@ class Anime(object):
         return self._loaded
 
     def load(self):
-        """
+        """Load all extra information for this anime.
+
+        The anidb url should look like this:
         http://api.anidb.net:9001/httpapi?request=anime&client={str}&clientver={int}&protover=1&aid={int}
-        :return:
         """
         params = {
             "request": "anime",
@@ -109,6 +110,32 @@ class BaseAttribute(object):
         self.anime = anime
         self._xml = xml_node
 
+    def _attributes(self, *attrs):
+        """Set the given attributes.
+
+        :param list attrs: the attributes to be set.
+        """
+        for attr in attrs:
+            setattr(self, attr, self._xml.attrib.get(attr))
+
+    def _booleans(self, *attrs):
+        """Set the given attributes after casting them to bools.
+
+        :param list attrs: the attributes to be set.
+        """
+        for attr in attrs:
+            value = self._xml.attrib.get(attr)
+            setattr(self, attr, value is not None and value == "true")
+
+    def _texts(self, *attrs):
+        """Set the text values of the given attributes.
+
+        :param list attrs: the attributes to be found.
+        """
+        for attr in attrs:
+            value = self._xml.find(attr)
+            setattr(self, attr, value.text if value is not None else None)
+
     def __str__(self):
         return self._xml.text
 
@@ -118,35 +145,32 @@ class BaseAttribute(object):
             unicode(self)
         )
 
+
 class Category(BaseAttribute):
 
     def __init__(self, anime, xml_node):
         super(Category, self).__init__(anime, xml_node)
-        self.id = self._xml.attrib["id"]
-        self.hentai = self._xml.attrib["hentai"] == "true"
-        self.weigth = self._xml.attrib["weight"]
-        self.name = self._xml.find("name").text
-        self.description = self._xml.find("description").text
+        self._attributes('id', 'weight')
+        self._booleans('hentai')
+        self._texts('name', 'description')
+
 
 class Tag(BaseAttribute):
 
     def __init__(self, anime, xml_node):
         super(Tag, self).__init__(anime, xml_node)
-        self.id = self._xml.attrib["id"]
-        self.spoiler = self._xml.attrib["spoiler"] == "true"
-        self.localspoiler = self._xml.attrib["localspoiler"] == "true"
-        self.globalspoiler = self._xml.attrib["globalspoiler"] == "true"
-        self.update = date_to_date(self._xml.attrib["update"])
-        self.name = self._xml.find("name").text
-        self.description = None
-        if self._xml.find("description") is not None:
-            self.description = self._xml.find("description").text
-        self.count = int(self._xml.find("count").text)
+        self._attributes('id', 'update')
+        if self.update:
+            self.update = date_to_date(self.update)
+
+        self._booleans('spoiler', 'localspoiler', 'globalspoiler')
+        self._texts('name', 'description', 'weight')
+        self.count = int(self.weight) if self.weight else 0
+        """The importance of this tag."""
 
     def __cmp__(self, other):
-        if self.count > other.count:
-            return 1
-        return -1
+        return self.count - other.count
+
 
 class Title(BaseAttribute):
 
@@ -155,6 +179,7 @@ class Title(BaseAttribute):
         # apperently xml:lang is "{http://www.w3.org/XML/1998/namespace}lang"
         self.lang = self._xml.attrib["{http://www.w3.org/XML/1998/namespace}lang"]
         self.type = self._xml.attrib.get("type")
+
 
 class Picture(BaseAttribute):
 
@@ -170,17 +195,14 @@ class Episode(BaseAttribute):
 
     def __init__(self, anime, xml_node):
         super(Episode, self).__init__(anime, xml_node)
-        self.id = int(self._xml.attrib["id"])
+        self._attributes('id')
+        self._texts('airdate', 'length', 'epno')
+
         self.titles = [Title(self, n) for n in self._xml.findall("title")]
-        try:
-            self.airdate = date_to_date(self._xml.find("airdate").text)
-        except (AttributeError, TypeError):
-            self.airdate = None
         self.type = int(self._xml.find("epno").attrib["type"])
-        self.number = self._xml.find("epno").text
+        self.number = self.epno or 0
         if self.type == 1:
             self.number = int(self.number)
-        self.length = self._xml.find("length").text
 
     @property
     def title(self):
@@ -205,8 +227,4 @@ class Episode(BaseAttribute):
         if self.number < other.number:
             return -1
         return 1
-
-
-
-
 
